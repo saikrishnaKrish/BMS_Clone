3f9df1267bc0b80d1917f85fc6b3d0e2ef613c2c
Sai Krishna Kanteti <saikrishna.kanteti@gmail.com>
2024-01-19 20:18:24 +0530

    updated db config file with event listeners

--- Server/DBConfig/dbconfig.js
+++ Server/DBConfig/dbconfig.js
@@ -1,9 +1,40 @@
-const {username,password}=require('../cridentials')
-const mongo_url=`mongodb+srv://${username}:${password}@cluster0.frkiogv.mongodb.net/?retryWrites=true&w=majority`;
+const { username, password } = require('../credentials');
 
-console.log(mongo_url)
+// Construct MongoDB connection URL
+const mongo_url = `mongodb+srv://${username}:${password}@cluster0.frkiogv.mongodb.net/?retryWrites=true&w=majority`;
+
+console.log('MongoDB Connection URL:', mongo_url);
 
 const mongoose = require('mongoose');
-mongoose.connect(mongo_url)
-.then(()=>console.log('connected to mongodb'))
-.catch(()=>console.log('unable to connect to DB'))
+
+// Connect to MongoDB
+mongoose.connect(mongo_url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error.message);
+  });
+
+// Event listeners for MongoDB connection events
+const connection = mongoose.connection;
+
+connection.on('connected', () => {
+  console.log('Mongoose connected to MongoDB');
+});
+
+connection.on('error', (err) => {
+  console.error('Mongoose connection error:', err);
+});
+
+connection.on('disconnected', () => {
+  console.log('Mongoose disconnected from MongoDB');
+});
+
+// Handling application termination
+process.on('SIGINT', () => {
+  connection.close(() => {
+    console.log('Mongoose connection disconnected through app termination');
+    process.exit(0);
+  });
+});
