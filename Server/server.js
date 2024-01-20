66c41b8c576daa5c8587d020795244731b944bd1
Sai Krishna Kanteti <saikrishna.kanteti@gmail.com>
2024-01-20 11:57:27 +0530

    clean up

--- Server/server.js
+++ Server/server.js
@@ -6,14 +6,12 @@ const dbConfig=require('./DBConfig/dbconfig');
 
 
 const userRoutes = require('./routes/userRoutes');
-const bottleRoutes = require('./routes/bottleRoutes');
 
 //Middlewares
 app.use(express.json())
 
 //Routes
 app.use('/api/users',userRoutes.router);
-app.use('/api/bottles',bottleRoutes.router);
 
 
 const PORT=8080;
