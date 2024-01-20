d62cc6a5138f9c1ca208cd1556b4385e312d4eae
Sai Krishna Kanteti <saikrishna.kanteti@gmail.com>
2024-01-20 17:02:58 +0530

    added login and register route

---
+++ Client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import AppRouter from './routes/AppRouter'
+
+function App() {
+
+
+  return (<>
+      <AppRouter/>
+    </>
+  )
+}
+
+export default App
