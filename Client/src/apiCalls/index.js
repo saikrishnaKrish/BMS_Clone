dc1443ce25f5f4fac8b993522c2ff77bd39fa1d2
Sai Krishna Kanteti <saikrishna.kanteti@gmail.com>
2024-01-23 00:38:00 +0530

    fixed token issue in the headers

--- Client/src/apiCalls/index.js
+++ Client/src/apiCalls/index.js
@@ -1,11 +1,25 @@
 import axios from "axios";
 
-const token = localStorage.getItem("token");
+// Create a function to generate the Axios instance with dynamic headers
+export const createAxiosInstance = () => {
+  const instance = axios.create({
+    headers: {
+      credentials: 'include',
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+  });
 
-export const axiosInstance = axios.create({
-  headers: {
-    cridentials:'include',
-    'Content-Type':"application/json",
-    Authorization: "Bearer " + token,
-  },
-});
+  // Interceptor to update the Authorization header for each request
+  instance.interceptors.request.use((config) => {
+    config.headers.Authorization = `Bearer ${localStorage.getItem("token")}`;
+    return config;
+  });
+
+  return instance;
+};
+
+// Usage in your component or module
+const axiosInstance = createAxiosInstance();
+
+export default axiosInstance;
